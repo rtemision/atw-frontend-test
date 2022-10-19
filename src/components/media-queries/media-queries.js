eefe56962709f76a2afcde0f69c5fe284b3dba7d
rtemision <3108511+rtemision@users.noreply.github.com>
2022-10-19 17:38:57 +0700

    change custom-media breakpoints

--- src/components/media-queries/media-queries.js
+++ src/components/media-queries/media-queries.js
@@ -1,17 +1,9 @@
 module.exports = {
-  'lg-min': '(min-width: 1640px)',
+  'md-min': '(min-width: 1120px)',
 
-  'md-only': '(min-width: 1280px) and (max-width: 1639px)',
-  'md-max': '(max-width: 1639px)',
-  'md-min': '(min-width: 1280px)',
-
-  'sm-only': '(min-width: 768px) and (max-width : 1279px)',
-  'sm-max': '(max-width: 1279px)',
+  'sm-only': '(min-width: 768px) and (max-width : 1119px)',
+  'sm-max': '(max-width: 1119px)',
   'sm-min': '(min-width: 768px)',
 
-  'xs-only': '(min-width: 450px) and (max-width : 767px)',
-  'xs-max': '(max-width: 767px)',
-  'xs-min': '(min-width: 450px)',
-
-  'xxs-max': '(max-width: 449px)'
+  'xs-max': '(max-width: 767px)'
 };
